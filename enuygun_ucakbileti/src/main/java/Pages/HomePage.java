a72949d139650a6d826de416438f19732cea1a7e
Ozan Cetinyol <ozan.cetinyol@enuygun.com>
2024-03-04 16:38:49 +0300

    seçimler parametrik yapıldı, havayolu secim filtre metodu eklendi

--- enuygun_ucakbileti/src/main/java/Pages/HomePage.java
+++ enuygun_ucakbileti/src/main/java/Pages/HomePage.java
@@ -1,17 +1,20 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+
 import java.util.List;
 
 
 public class HomePage {
 
     private WebDriver driver;
+
     public HomePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
@@ -59,48 +62,62 @@ public class HomePage {
     @FindBy(xpath = "//div[@class='flight-departure-time']")
     List<WebElement> departureTimes;
 
+    @FindBy(css = "[class='ctx-filter-airline card-header']")
+    WebElement openAirlineFilter;
+
+    @FindBy(xpath = "(//*[@class='filter-label'])[5]")
+    WebElement chooseAirlineFilter;
+
+    @FindBy(css = "[class='money-int']")
+    List<WebElement> priceElements;
+
 
     public void goToEnuygunHomepage() {
         driver.get("https://www.enuygun.com/");
     }
 
-    public void fromWhereToWhere() throws InterruptedException {
+    public void fromWhereToWhere(String nereden, String nereye) throws InterruptedException {
 
         roundTrip.click();
         fromWhere.click();
-        fromWhere.sendKeys("İstanbul");
+        fromWhere.sendKeys(nereden);
         Thread.sleep(1000);
         fromWhereClick.click();
         toWhere.click();
-        toWhere.sendKeys("Ankara");
+        toWhere.sendKeys(nereye);
         Thread.sleep(1000);
         toWhereClick.click();
     }
 
-    public void datePicker() {
+    public void datePicker(String gidisTarihi, String dönüsTarihi) throws InterruptedException {
         departureDate.click();
+        WebElement departureDatePicker = driver.findElement(By.xpath("(//*[@class='sc-gdfaqJ fjsa-dD'])[1] //*[@class='sc-iZzKWI dlKEMh'] //*[@class='sc-cQCQeq hWjVug'] //*[@data-day='" + gidisTarihi + "']"));
+        departureDatePicker.click();
         returnDate.click();
+        WebElement returnDatePicker = driver.findElement(By.xpath("(//*[@class='sc-gdfaqJ fjsa-dD'])[1] //*[@class='sc-iZzKWI dlKEMh'] //*[@class='sc-cQCQeq hWjVug'] //*[@data-day='" + dönüsTarihi + "']"));
+        returnDatePicker.click();
+
     }
 
     public void searchButton() throws InterruptedException {
         searchButton.click();
         Thread.sleep(5000);
-        Assert.assertTrue(addFavoriteButton.isDisplayed(),"Sayfa yüklenirken bir hata oluştu!!!");
+        Assert.assertTrue(addFavoriteButton.isDisplayed(), "Sayfa yüklenirken bir hata oluştu!!!");
         System.out.println("Sayfa başarıyla yüklendi");
     }
 
-    public void chooseFilter() throws InterruptedException {
+    public void chooseDepartureTimeFilter() throws InterruptedException {
         Actions sliderAction = new Actions(driver);
         departureTimeFilter.click();
         int xOffsetStart = 100;
         int xOffsetEnd = -60;
         sliderAction.dragAndDropBy(startPointOfSlider, xOffsetStart, 0).build().perform();
         Thread.sleep(2000);
-        sliderAction.dragAndDropBy(endPointOfSlider, xOffsetEnd,0).build().perform();
+        sliderAction.dragAndDropBy(endPointOfSlider, xOffsetEnd, 0).build().perform();
         Thread.sleep(2000);
     }
 
-    public void checkFilter(){
+    public void checkFilter() {
         boolean isWithinRange = true;
         for (WebElement departureTime : departureTimes) {
             String timeString = departureTime.getText();
@@ -110,14 +127,27 @@ public class HomePage {
                 isWithinRange = false;
                 break;
             }
+
+            System.out.println("Kalkış saatleri: " + hour);
+
         }
+
 
         if (isWithinRange) {
             System.out.println("Tüm uçuşların kalkış saatleri 10:00 ile 18:00 arasında.");
         } else {
             System.out.println("Bazı uçuşların kalkış saatleri 10:00 ile 18:00 arasında değil.");
         }
-
     }
 
+    public void chooseAirlineFilter() throws InterruptedException {
+        openAirlineFilter.click();
+        Thread.sleep(2000);
+        chooseAirlineFilter.click();
+        Thread.sleep(2000);
+    }
+
+    public void checkPriceThy() {
+
+    }
 }
--- enuygun_ucakbileti/src/test/java/Test.java
+++ enuygun_ucakbileti/src/test/java/Test.java
@@ -1,18 +1,17 @@
 import Pages.HomePage;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class Test {
 
     private WebDriver driver;
     private HomePage homePage;
 
-    @BeforeTest
+    @BeforeMethod
     public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -23,25 +22,27 @@ public class Test {
     @org.testng.annotations.Test
     public void case1() throws InterruptedException {
         homePage.goToEnuygunHomepage();
-        homePage.fromWhereToWhere();
-        homePage.datePicker();
+        homePage.fromWhereToWhere("İstanbul", "Ankara");
+        homePage.datePicker("20", "21");
         homePage.searchButton();
-        homePage.chooseFilter();
+        homePage.chooseDepartureTimeFilter();
         homePage.checkFilter();
     }
 
     @org.testng.annotations.Test
     public void case2() throws InterruptedException {
         homePage.goToEnuygunHomepage();
-        homePage.fromWhereToWhere();
-        homePage.datePicker();
+        homePage.fromWhereToWhere("İstanbul", "Ankara");
+        homePage.datePicker("24", "25");
         homePage.searchButton();
-
+        homePage.chooseDepartureTimeFilter();
+        homePage.checkFilter();
+        homePage.chooseAirlineFilter();
+        homePage.checkPriceThy();
     }
 
-    @AfterTest
-    public void tearDown() {
-        // WebDriver'ı kapat
+    @AfterMethod
+    public void afterMethod() {
         driver.quit();
     }
 
